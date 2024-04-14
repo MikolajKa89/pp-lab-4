9c2d13502f6cb9543e85690b10170dcf3db564c1
Mikołaj Karczmarczy <miczi@MacBook-Air-Mikoaj.local>
2024-04-14 12:33:05 +0200

    zmiana

--- Company.java
+++ Company.java
@@ -1,21 +1,21 @@
-class Company {
+public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
-        employees[0] = new Employee("Tom Henks", 50000);
-        employees[1] = new Employee("", 60000);
+        employees[0] = new Manager("Tom Henks", 50000);
+        employees[1] = new Worker("", 60000);
         employees[2] = new Employee("Leonardo DiCaprio", 70000);
-        employees[3] = new Employee("Robert Downey Jr.", 55000);
-        employees[4] = new Employee("Brad Pitt", 65000);
+        employees[3] = new Worker("Robert Downey Jr.", 55000);
+        employees[4] = new Worker("Brad Pitt", 65000);
 
-        // Wyświetlenie danych dla pracownika o indeksie równym 3
-        System.out.println("Dane dla pracownika o indeksie 3:");
-        System.out.println(employees[3]);
+        // Dodawanie pracowników specyficznych dla zadania
+        employees[0].setNumberOfSubordinates(0); // Menedżer - brak podwładnych
 
-        // Modyfikacja salary dla pracownika o indeksie równym 3
-        employees[3].setSalary(60000);
+        // Zamiana pracowników o indeksach 1 i 4 na instancje klasy Worker
+        employees[1] = new Worker("", 60000);
+        employees[4] = new Worker("Brad Pitt", 65000);
 
         // Wyświetlenie danych dla wszystkich pracowników
-        System.out.println("\nDane dla wszystkich pracowników:");
+        System.out.println("Dane dla wszystkich pracowników:");
         for (Employee employee : employees) {
             System.out.println(employee);
         }
